8ae0f2dc3081ee5f6b3c817ae6e88d0de924c78d
Chad <107705900+carrchadd@users.noreply.github.com>
2024-02-03 18:58:47 -0500

    Installed shadcn

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import Head from 'next/head';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hellooooo!
+      <Card className="bg-primary">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
