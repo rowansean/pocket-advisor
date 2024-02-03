77844339500a998458b8a0995b2a6c113d8d6914
Sean-Patrick <rowan.seanp@gmail.com>
2024-02-03 17:30:45 -0500

    Added Global CSS Variables and config with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'pink-primary': '#FABCFF',
+        'pink-secondary': '#EB00FF',
+        'blue-primary': '#7FB2FF',
+        'blue-secondary': '#0047FF',
+        'green-primary': '#9BE7A7',
+        'green-secondary': '#00D923',
+        'yellow-primary': '#FFD97D',
+        'yellow-secondary': '#FFA800',
+      },
     },
   },
   plugins: [],
