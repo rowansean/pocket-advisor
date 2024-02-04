7e7e881a50ba4b468d1ea4d6a0e0639a89842a60
GavinGMC <90469511+GavinGMC@users.noreply.github.com>
2024-02-03 19:27:43 -0500

    fixing mistakes

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,9 @@
 import Image from "next/image";
-import Head from 'next/head';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Card className="bg-primary">
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
+      Hellooooo!
     </main>
   );
 }
